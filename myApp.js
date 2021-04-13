cdb2b629a643aa44b00dc9462ee675b3e986910e
My name <me@example.com>
2021-04-13 17:10:02 +0300

    added GET handler with middleware for path '/now'

--- myApp.js
+++ myApp.js
@@ -18,7 +18,10 @@ app.get("/json", (req, res) => {
          : {"message": "HELLO JSON"});
 });
 
-
+app.get("/now", (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+}, (req, res) => res.json({"time": req.time}));
 
 
 
