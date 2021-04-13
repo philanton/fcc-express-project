055f8b521839e3d50834eb7f5ef1dd2e36a044b1
My name <me@example.com>
2021-04-13 16:08:59 +0300

    added GET handler to path '/' with HTML file

--- myApp.js
+++ myApp.js
@@ -3,6 +3,7 @@ var app = express();
 
 console.log("Hello World");
 
+app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 app.get("/", (req, res) => res.send("Hello Express"));
 
 
