5a2d0e1f1a0e41325fbee51bd8975f9f1688bff4
My name <me@example.com>
2021-04-13 16:16:07 +0300

    added path to other files containing static page info

--- myApp.js
+++ myApp.js
@@ -3,6 +3,8 @@ var app = express();
 
 console.log("Hello World");
 
+app.use(express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 app.get("/", (req, res) => res.send("Hello Express"));
 
